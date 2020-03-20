35ea19c6a0bee69f3f540c71696f190a5048ec3e
Valentin Hörzi <valentin.hoerzi@gmail.com>
2020-03-21 00:59:18 +0100

    Worked on garages.fxml

--- src/main/java/net/hoerzintelligence/mygarages/controller/GaragesController.java
+++ src/main/java/net/hoerzintelligence/mygarages/controller/GaragesController.java
@@ -2,31 +2,59 @@ package net.hoerzintelligence.mygarages.controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import net.hoerzintelligence.mygarages.data.car.CarResource;
 import net.hoerzintelligence.mygarages.data.garage.GarageResource;
 import net.hoerzintelligence.mygarages.services.Model;
 
+import java.net.PortUnreachableException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class GaragesController implements Initializable {
     private final Model model = Model.getInstance();
     private GarageResource selectedGarage = null;
+
+    ObservableList<GarageResource> garageData = null;
+    ObservableList<CarResource> carData = null;
+
     @FXML
     private TableView<GarageResource> tableGarages;
-    ObservableList<GarageResource> data = null;
     @FXML
-    private TableColumn<GarageResource,Integer> columnId;
+    private TableColumn<GarageResource, Integer> columnId;
     @FXML
-    private TableColumn<GarageResource,String> columnName;
+    private TableColumn<GarageResource, String> columnName;
     @FXML
-    private TableColumn<GarageResource,String > columnAddress;
+    private TableColumn<GarageResource, String> columnAddress;
+    @FXML
+    private TableView<CarResource> tableCars;
+    @FXML
+    private TableColumn<CarResource, Integer> columnCarId;
+    @FXML
+    private TableColumn<CarResource, String> columnbrandAndName;
+    @FXML
+    private TableColumn<CarResource, String> columnPS;
+    @FXML
+    private TextField txtGarageAddress;
+    @FXML
+    private TextField txtGarageName;
+    @FXML
+    private TextField txtCarPS;
+    @FXML
+    private TextField txtCarName;
+    @FXML
+    private TextField txtCarBrand;
+    @FXML
+    private Label lblSelectedGarage;
 
 
     @Override
@@ -34,7 +62,17 @@ public class GaragesController implements Initializable {
         columnId.setCellValueFactory(new PropertyValueFactory<>("id"));
         columnName.setCellValueFactory(new PropertyValueFactory<>("name"));
         columnAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
-        data = FXCollections.observableArrayList(model.getAllGarages());
+        columnCarId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        columnbrandAndName.setCellValueFactory(new PropertyValueFactory<>("brandAndName"));
+        columnPS.setCellValueFactory(new PropertyValueFactory<>("horsePower"));
+
+        initData();
+    }
+
+    private void initData() {
+        List<GarageResource> garages = model.getAllGarages();
+        garageData = FXCollections.observableArrayList(garages);
+        carData = FXCollections.observableArrayList();
         updateTable();
     }
 
@@ -44,7 +82,26 @@ public class GaragesController implements Initializable {
 
     }
 
-    private void updateTable(){
-        tableGarages.setItems(data);
+    private void updateTable() {
+        tableGarages.setItems(garageData);
+        tableCars.setItems(carData);
+    }
+
+    @FXML
+    private void onButtonAddGarage(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onButtonAddCar(ActionEvent actionEvent) {
+    }
+
+    private void clearGarageTextFields(){
+        txtGarageAddress.setText("");
+        txtGarageName.setText("");
+    }
+    private void clearCarTextFields(){
+        txtCarBrand.setText("");
+        txtCarName.setText("");
+        txtCarPS.setText("");
     }
 }
